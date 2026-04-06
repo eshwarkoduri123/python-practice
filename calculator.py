7085cd53ac976fd1a70d5c4ad6925d64d7be9ae5
eshwarkoduri123 <kodurieshwar13@gmail.com>
2026-04-06 19:29:56 +0530

    added calculator file

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# x = int(input("value of x : "))
+# y = int(input("value of y : "))
+
+# z = x+y
+
+# print(z)
+
+# print(float(input("value of x : ")) + float(input("value of y : ")))
+
+x = float(input("value of x : "))
+y = float(input("value of y : "))
+
+z = x / y
+
+print(z)
